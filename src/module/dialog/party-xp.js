36bad7d655fe88be371dd48f46c27f952c25b3ec
freohr <faure.stephen@gmail.com>
2022-01-16 12:02:49 +0100

    Correctly close XP form on submit

--- src/module/dialog/party-xp.js
+++ src/module/dialog/party-xp.js
@@ -6,6 +6,7 @@ export class OsePartyXP extends FormApplication {
       width: 300,
       height: "auto",
       resizable: false,
+      closeOnSubmit: true
     });
   }
 
@@ -55,6 +56,10 @@ export class OsePartyXP extends FormApplication {
   }
   /* -------------------------------------------- */
 
+  _updateObject(event, formData) {
+    this._dealXP(event);
+  }
+
   _calculateShare(ev) {
     const actors = this.object.documents.filter(
       (e) =>
@@ -93,6 +98,7 @@ export class OsePartyXP extends FormApplication {
 
     const totalField = html.find('input[name="total"]');
     totalField.on("input", this._calculateShare.bind(this));
-    html.find('button[data-action="deal-xp"').click(this._dealXP.bind(this));
+
+    html.find('button[data-action="deal-xp"').click(event => { super.submit(event) });
   }
 }

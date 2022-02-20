99f85a8ee3ee28bbcefba6df2fd9b585e8033346
Anthony Ronda <anthonyronda@gmail.com>
2022-02-19 20:47:02 -0500

    fix: some items properly filtered out of ItemPiles

--- src/module/fvttModuleAPIs.js
+++ src/module/fvttModuleAPIs.js
@@ -4,8 +4,23 @@ export const registerFVTTModuleAPIs = () => {
     Hooks.once("item-piles-ready", async function () {
       if (ItemPiles.API.ACTOR_CLASS_TYPE !== "character")
         ItemPiles.API.setActorClassType("character");
-      if (!ItemPiles.API.ITEM_QUANTITY_ATTRIBUTE !== "data.quantity.value")
+      if (ItemPiles.API.ITEM_QUANTITY_ATTRIBUTE !== "data.quantity.value")
         ItemPiles.API.setItemQuantityAttribute("data.quantity.value");
+      if (
+        ItemPiles.API.ITEM_FILTERS !==
+        [
+          {
+            path: "type",
+            filters: "spell,ability",
+          },
+        ]
+      )
+        ItemPiles.API.setItemFilters([
+          {
+            path: "type",
+            filters: "spell,ability",
+          },
+        ]);
     });
   }
 };

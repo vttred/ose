0ed582b877bc0d526a123c7f1dede26fe10cb20b
RabidOwlbear <owlbearaviary@gmail.com>
2022-03-16 18:36:57 -0700

    Fix: tweaks icon not showing for non gm users

--- src/module/actor/actor-sheet.js
+++ src/module/actor/actor-sheet.js
@@ -356,7 +356,7 @@ export class OseActorSheet extends ActorSheet {
     let buttons = super._getHeaderButtons();
 
     // Token Configuration
-    const canConfigure = game.user.isGM || this.actor.owner;
+    const canConfigure = game.user.isGM || this.actor.isOwner;
     if (this.options.editable && canConfigure) {
       buttons = [
         {

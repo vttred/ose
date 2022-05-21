b036d660851f1aed25bd9ad028bc02e882e05ad7
RabidOwlbear <owlbearaviary@gmail.com>
2022-05-21 16:29:37 -0700

    ability chat card render fix

--- src/module/combat.js
+++ src/module/combat.js
@@ -14,6 +14,7 @@ export class OseCombat {
     };
   }
   static async rollInitiative(combat, data) {
+    console.log('roll init')
     // Check groups
     data.combatants = [];
     let groups = {};
@@ -63,6 +64,7 @@ export class OseCombat {
       return;
     }
     combat.resetAll();
+    
   }
 
   static async individualInitiative(combat, data) {
@@ -268,6 +270,7 @@ export class OseCombat {
         return;
       }
       let data = {};
+      console.log('bingo')
       OseCombat.rollInitiative(game.combat, data);
       if (game.user.isGM) {
         game.combat.update({ data: data }).then(() => {

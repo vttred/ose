8d81f38317e0750003fe7a8e66329c4150ffc6aa
Michael Schilling <maschill92@gmail.com>
2022-08-12 09:58:59 -0500

    Include rollup to define config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
+const { defineConfig } = require("rollup");
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const typescript = require("@rollup/plugin-typescript");
 
-module.exports = () => ({
-  input: "src/ose.js",
-  output: {
-    dir: "dist/",
-    format: "es",
-    sourcemap: true,
-  },
-  plugins: [nodeResolve(), typescript()],
-});
+module.exports = () =>
+  defineConfig({
+    input: "src/ose.js",
+    output: {
+      dir: "dist/",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [nodeResolve(), typescript()],
+  });

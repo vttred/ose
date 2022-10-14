87c8e847736dbba402a241753c107a88f3957452
wyrmisis <tim.snyder.work@gmail.com>
2022-10-13 21:42:25 -0400

    feat: Apply the character data model feat: Lock the character actor type out from prepareData calls feat: remove item prep code from the character sheet feat: Minor template field remapping

--- src/ose.js
+++ src/ose.js
@@ -18,6 +18,8 @@ import * as renderList from "./module/renderList";
 import { OsePartySheet } from "./module/party/party-sheet";
 import './e2e';
 
+import OseDataModelCharacter from './module/actor/data-model-character';
+
 /* -------------------------------------------- */
 /*  Foundry VTT Initialization                  */
 /* -------------------------------------------- */
@@ -53,6 +55,8 @@ Hooks.once("init", async function () {
 
   CONFIG.Actor.documentClass = OseActor;
   CONFIG.Item.documentClass = OseItem;
+
+  CONFIG.Actor.systemDataModels['character'] = OseDataModelCharacter;
 
   // Register sheet application classes
   Actors.unregisterSheet("core", ActorSheet);

4e0ec00df6035b5bb6084c571c049805b03a6d0c
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-01-17 06:46:58 +0100

    cleanup

--- src/module/behaviourHelpers.js
+++ src/module/behaviourHelpers.js
@@ -1,8 +1,5 @@
 export const skipRollDialogCheck = (event) => {
     const invertedCtrlBehavior = game.settings.get(game.system.id, "invertedCtrlBehavior");
-
-    console.log('Helper triggered.')
-
     return invertedCtrlBehavior ? 
         !(event && (event.ctrlKey || event.metaKey))
         :

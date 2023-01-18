7887267463ccb0d40332d44ffc38b233dae3e393
wyrmisis <tim.snyder.work@gmail.com>
2023-01-18 13:11:50 -0500

    chore: Fix ose.js per linting rules

--- src/ose.js
+++ src/ose.js
@@ -1,40 +1,46 @@
+/**
+ * @file The entry point for the OSE system
+ *       We should handle most of our setup here.
+ */
+
 // Import Modules
-import { OseItemSheet } from "./module/item/item-sheet";
-import { OseActorSheetCharacter } from "./module/actor/character-sheet";
-import { OseActorSheetMonster } from "./module/actor/monster-sheet";
-import { preloadHandlebarsTemplates } from "./module/preloadTemplates";
-import { OseActor } from "./module/actor/entity";
-import { OseItem } from "./module/item/entity";
-import { OSE } from "./module/config";
-import { registerSettings } from "./module/settings";
-import { registerHelpers } from "./module/helpers";
-import { registerFVTTModuleAPIs } from "./module/fvttModuleAPIs";
-import * as chat from "./module/chat";
-import * as treasure from "./module/treasure";
-import * as macros from "./module/macros";
-import * as party from "./module/party";
-import { OseCombat } from "./module/combat";
-import * as renderList from "./module/renderList";
-import { OsePartySheet } from "./module/party/party-sheet";
 import './e2e';
 
+import { OseActorSheetCharacter } from "./module/actor/character-sheet";
 import OseDataModelCharacter from './module/actor/data-model-character';
 import OseDataModelMonster from './module/actor/data-model-monster';
-import OseDataModelWeapon from './module/item/data-model-weapon';
+import { OseActor } from "./module/actor/entity";
+import { OseActorSheetMonster } from "./module/actor/monster-sheet";
+import * as chat from "./module/chat";
+import { OseCombat } from "./module/combat";
+import { OSE } from "./module/config";
+import { registerFVTTModuleAPIs } from "./module/fvttModuleAPIs";
+import { registerHelpers } from "./module/helpers";
+import OseDataModelAbility  from './module/item/data-model-ability';
 import OseDataModelArmor from './module/item/data-model-armor';
+import OseDataModelContainer from './module/item/data-model-container';
 import OseDataModelItem from './module/item/data-model-item';
 import OseDataModelSpell from './module/item/data-model-spell';
-import OseDataModelAbility  from './module/item/data-model-ability';
-import OseDataModelContainer from './module/item/data-model-container';
+import OseDataModelWeapon from './module/item/data-model-weapon';
+import { OseItem } from "./module/item/entity";
+import { OseItemSheet } from "./module/item/item-sheet";
+import * as macros from "./module/macros";
+import * as party from "./module/party";
+import { OsePartySheet } from "./module/party/party-sheet";
+import { preloadHandlebarsTemplates } from "./module/preloadTemplates";
+import * as renderList from "./module/renderList";
+import { registerSettings } from "./module/settings";
+import * as treasure from "./module/treasure";
 
 /* -------------------------------------------- */
 /*  Foundry VTT Initialization                  */
 /* -------------------------------------------- */
 
-Hooks.once("init", async function () {
+Hooks.once("init", async () => {
   /**
    * Set an initiative formula for the system
-   * @type {String}
+   * 
+   * @type {string}
    */
   CONFIG.Combat.initiative = {
     formula: "1d6 + @init",
@@ -106,28 +112,29 @@ Hooks.once("init", async function () {
 /**
  * This function runs after game data has been requested and loaded from the servers, so entities exist
  */
-Hooks.once("setup", function () {
+Hooks.once("setup", () => {
   // Localize CONFIG objects once up-front
-  const toLocalize = [
+  [
     "saves_short",
     "saves_long",
     "scores",
     "armor",
     "colors",
     "tags",
-  ];
-  for (let o of toLocalize) {
+  ].forEach((o) => {
     CONFIG.OSE[o] = Object.entries(CONFIG.OSE[o]).reduce((obj, e) => {
-      obj[e[0]] = game.i18n.localize(e[1]);
-      return obj;
+      const localized = {...obj}
+      localized[e[0]] = game.i18n.localize(e[1]);
+      return localized;
     }, {});
-  }
+  });
 
   // Custom languages
   const languages = game.settings.get(game.system.id, "languages");
-  if (languages != "") {
-    const langArray = languages.split(",");
-    langArray.forEach((l, i) => (langArray[i] = l.trim()));
+  if (languages !== "") {
+    const langArray = languages
+      .split(",")
+      .map(s => s.trim());
     CONFIG.OSE.languages = langArray;
   }
 });
@@ -144,9 +151,9 @@ Hooks.on("renderSidebarTab", async (object, html) => {
     party.addControl(object, html);
   }
   if (object instanceof Settings) {
-    let gamesystem = html.find("#game-details");
+    const gamesystem = html.find("#game-details");
     // SRD Link
-    let ose = gamesystem.find("h4").last();
+    const ose = gamesystem.find("h4").last();
     ose.append(
       ` <sub><a href="https://oldschoolessentials.necroticgnome.com/srd/index.php">SRD<a></sub>`
     );
@@ -157,13 +164,13 @@ Hooks.on("renderSidebarTab", async (object, html) => {
     gamesystem.find(".system").append(rendered);
 
     // User guide
-    let docs = html.find("button[data-action='docs']");
+    const docs = html.find("button[data-action='docs']");
     const styling =
       "border:none;margin-right:2px;vertical-align:middle;margin-bottom:5px";
     $(
       `<button type="button" data-action="userguide"><img src='${OSE.assetsPath}/dragon.png' width='16' height='16' style='${styling}'/>Old School Guide</button>`
     ).insertAfter(docs);
-    html.find('button[data-action="userguide"]').click((ev) => {
+    html.find('button[data-action="userguide"]').click(() => {
       new FrameViewer("https://vttred.github.io/ose", {
         resizable: true,
       }).render(true);
@@ -172,7 +179,7 @@ Hooks.on("renderSidebarTab", async (object, html) => {
 });
 
 Hooks.on("preCreateCombatant", (combat, data, options, id) => {
-  let init = game.settings.get(game.system.id, "initiative");
+  const init = game.settings.get(game.system.id, "initiative");
   if (init === "group") {
     OseCombat.addCombatant(combat, data, options, id);
   }
@@ -186,7 +193,7 @@ Hooks.on(
   OseCombat.debounce(OseCombat.addContextEntry, 100)
 );
 
-Hooks.on("renderChatLog", (app, html, data) => OseItem.chatListeners(html));
+Hooks.on("renderChatLog", (app, html) => OseItem.chatListeners(html));
 Hooks.on("getChatLogEntryContext", chat.addChatMessageContextOptions);
 Hooks.on("renderChatMessage", chat.addChatMessageButtons);
 Hooks.on("renderRollTableConfig", treasure.augmentTable);

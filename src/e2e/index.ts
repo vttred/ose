dfc5b653839296f7bf15a19d068820e2e0b0d56c
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-01-29 11:12:35 +0100

    linted e2e/index.ts

--- src/e2e/index.ts
+++ src/e2e/index.ts
@@ -1,58 +1,48 @@
+/* eslint-disable import/no-cycle, simple-import-sort/imports */
+
 /**
  * @file Orchestration for our Quench tests
  */
-import actorCrudInventoryContainerTests, {
-  key as actorCrudInventoryContainerKey,
-  options as actorCrudInventoryContainerOptions
-} from '../module/actor/__tests__/character-crud-inventory-container.test';
-
-import macroTests, {
-  key as macroKey,
-  options as macroOptions
-} from '../module/__tests__/macros.test';
-
-import characterItemMacroTests, {
-  key as characterItemMacroKey,
-  options as characterItemMacroOptions
-} from '../module/actor/__tests__/character-macros.test';
-
-import characterTests, {
-  key as characterKey,
-  options as characterOptions
-} from './actor/character.e2e.test.js';
-
-import dataModelCharacterACTests, {
-  key as dataModelCharacterACKey,
-  options as dataModelCharacterACOptions,
-} from "../module/actor/data-model-classes/__tests__/data-model-character-ac.test";
-import dataModelCharacterEncumbranceTests, {
-  key as dataModelCharacterEncumbranceKey,
-  options as dataModelCharacterEncumbranceOptions,
-} from "../module/actor/data-model-classes/__tests__/data-model-character-encumbrance.test";
 import dataModelCharacterMoveTests, {
-  key as dataModelCharacterMoveKey,
+  key as dataModelCharacterMoveKey, 
   options as dataModelCharacterMoveOptions,
 } from "../module/actor/data-model-classes/__tests__/data-model-character-move.test";
-import dataModelCharacterScoresTests, {
-  key as dataModelCharacterScoresKey,
-  options as dataModelCharacterScoresOptions,
-} from "../module/actor/data-model-classes/__tests__/data-model-character-scores.test";
+import dataModelCharacterEncumbranceTests, {
+  key as dataModelCharacterEncumbranceKey, 
+  options as dataModelCharacterEncumbranceOptions,
+} from "../module/actor/data-model-classes/__tests__/data-model-character-encumbrance.test";
 import dataModelCharacterSpellsTests, {
   key as dataModelCharacterSpellsKey,
   options as dataModelCharacterSpellsOptions,
 } from "../module/actor/data-model-classes/__tests__/data-model-character-spells.test";
-import characterTests, {
-  key as characterKey,
-  options as characterOptions,
-} from "./actor/character.e2e.test.js";
+import dataModelCharacterScoresTests, {
+  key as dataModelCharacterScoresKey,
+  options as dataModelCharacterScoresOptions,
+} from "../module/actor/data-model-classes/__tests__/data-model-character-scores.test";
+import dataModelCharacterACTests, {
+  key as dataModelCharacterACKey,
+  options as dataModelCharacterACOptions,
+} from "../module/actor/data-model-classes/__tests__/data-model-character-ac.test";
+import dataModelCharacterTests, {
+  key as dataModelCharacterKey,
+  options as dataModelCharacterOptions,
+} from '../module/actor/data-model-character.test.js';
+import characterRollingTests, {
+  key as characterRollingKey,
+  options as characterRollingOptions,
+} from './actor/character-rolling.e2e.test';
+import actorCrudInventoryContainerTests, {
+  key as actorCrudInventoryContainerKey,
+  options as actorCrudInventoryContainerOptions,
+} from '../module/actor/__tests__/character-crud-inventory-container.test';
 import characterItemMacroTests, {
   key as characterItemMacroKey,
   options as characterItemMacroOptions,
-} from "./actor/createItemMacro.test";
-import dataModelCharacterTests, {
-  key as dataModelCharacterKey,
-  options as dataModelCharacterOptions
-} from '../module/actor/data-model-character.test.js';
+} from '../module/actor/__tests__/character-macros.test';
+import macroCreationTests, {
+  key as macroCreationKey,
+  options as macroCreationOptions,
+} from '../module/__tests__/macros-creation.test';
 
 export type QuenchMethods = {
   [s: string]: any;
@@ -63,11 +53,35 @@ type Quench = {
 };
 
 Hooks.on('quenchReady', async (quench: Quench) => {
-  quench.registerBatch(actorCrudInventoryContainerKey, actorCrudInventoryContainerTests, actorCrudInventoryContainerOptions);
-  quench.registerBatch(macroKey, macroTests, macroOptions);
-  quench.registerBatch(characterItemMacroKey, characterItemMacroTests, characterItemMacroOptions);
-  quench.registerBatch(characterKey, characterTests, characterOptions);
+  // Macro testing
+  quench.registerBatch(
+    macroCreationKey,
+    macroCreationTests,
+    macroCreationOptions
+  );
+  quench.registerBatch(
+    characterItemMacroKey,
+    characterItemMacroTests,
+    characterItemMacroOptions
+  );
+  // Actor CRUD testing
+  quench.registerBatch(
+    actorCrudInventoryContainerKey,
+    actorCrudInventoryContainerTests,
+    actorCrudInventoryContainerOptions
+  );
+  // Actor rolling testing
+  quench.registerBatch(
+    characterRollingKey,
+    characterRollingTests,
+    characterRollingOptions
+  );
   // Character data model classes
+  quench.registerBatch(
+    dataModelCharacterKey,
+    dataModelCharacterTests,
+    dataModelCharacterOptions
+  );
   quench.registerBatch(
     dataModelCharacterACKey,
     dataModelCharacterACTests,
@@ -92,10 +106,5 @@ Hooks.on('quenchReady', async (quench: Quench) => {
     dataModelCharacterMoveKey,
     dataModelCharacterMoveTests,
     dataModelCharacterMoveOptions
-  );
-  quench.registerBatch(
-    dataModelCharacterKey,
-    dataModelCharacterTests,
-    dataModelCharacterOptions
   );
 });

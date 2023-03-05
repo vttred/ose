14a41df4c1ea0bca82e0665577b3bed97696a700
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-03-05 09:14:47 +0100

    fix: defined type for key in cleanUpActorsByKey

--- src/e2e/testUtils.ts
+++ src/e2e/testUtils.ts
@@ -99,7 +99,7 @@ export const cleanUpMacros = async () => {
   return true;
 };
 
-export const cleanUpActorsByKey = async (key) => {
+export const cleanUpActorsByKey = async (key: string) => {
   game.actors
     ?.filter((a) => a.name === `Test Actor ${key}`)
     .forEach(async (a) => await a.delete());

5615806c9dc93e7eef5d3de0272787e684df790a
Amir Arad <greenshade@gmail.com>
2023-03-18 14:48:20 +0200

    hit original target of attack dont show context menu or button to apply damage unless possible

--- src/module/chat.ts
+++ src/module/chat.ts
@@ -1,5 +1,22 @@
 import { OseActor } from "./actor/entity";
 
+function canApplyDamage(html: JQuery) {
+  if (html.find(".dice-total").length)
+    switch (game.settings.get(game.system.id, "applyDamageOption")) {
+      case "originalTarget":
+        return html.find(".chat-target").last().data("id");
+      case "targeted":
+        return !!game.user?.targets?.size;
+      case "selected":
+        return !!canvas.tokens?.controlled.length;
+      default: {
+        console.log('unknown setting');
+        return false;
+      }
+    }
+  return false;
+}
+
 /**
  * This function is used to hook into the Chat Log context menu to add additional options to each message
  * These options make it easy to conveniently apply damage to controlled tokens based on the value of a Roll
@@ -8,8 +25,7 @@ export const addChatMessageContextOptions = function (
   _: JQuery,
   options: ContextMenuEntry[]
 ) {
-  let canApply: ContextMenuEntry["condition"] = (li) =>
-    !!canvas.tokens?.controlled.length && !!li.find(".dice-roll").length;
+  let canApply: ContextMenuEntry["condition"] = (li) => canApplyDamage(li) && !!li.find(".dice-roll").length;
   options.push(
     {
       name: game.i18n.localize("OSE.messages.applyDamage"),
@@ -45,7 +61,7 @@ export const addChatMessageButtons = function (msg: ChatMessage, html: JQuery) {
   }
   // Buttons
   let roll = html.find(".damage-roll");
-  if (roll.length > 0) {
+  if (roll.length > 0 && canApplyDamage(html)) {
     roll.append(
       $(
         `<div class="dice-damage"><button type="button" data-action="apply-damage"><i class="fas fa-tint"></i></button></div>`
@@ -53,7 +69,7 @@ export const addChatMessageButtons = function (msg: ChatMessage, html: JQuery) {
     );
     roll.find('button[data-action="apply-damage"]').on("click", (ev) => {
       ev.preventDefault();
-      applyChatCardDamage(roll, 1);
+      applyChatCardDamage(html, 1);
     });
   }
 };
@@ -62,29 +78,39 @@ export const addChatMessageButtons = function (msg: ChatMessage, html: JQuery) {
  * Apply rolled dice damage to the token or tokens which are currently controlled.
  * This allows for damage to be scaled by a multiplier to account for healing, critical hits, or resistance
  *
- * @param {HTMLElement} roll    The chat entry which contains the roll data
+ * @param {HTMLElement} html    The chat entry which contains the roll data
  * @param {Number} multiplier   A damage multiplier to apply to the rolled damage.
  * @return {Promise}
  */
-function applyChatCardDamage(roll: JQuery, multiplier: 1 | -1) {
-  const amount = roll.find(".dice-total").last().text();
-  const victimId = roll.find(".chat-target").last().data("id");
-  if (victimId) {
-    // TODO implement
-  } else {
-    const dmgTgt = game.settings.get(game.system.id, "applyDamageOption");
-    if (dmgTgt === `targeted`) {
-      game.user?.targets.forEach(async (t) => {
-        if (game.user?.isGM && t.actor instanceof OseActor)
-          await t.actor.applyDamage(amount, multiplier);
-      });
+function applyChatCardDamage(html: JQuery, multiplier: 1 | -1) {
+  const amount = html.find(".dice-total").last().text();
+  const dmgTgt = game.settings.get(game.system.id, "applyDamageOption");
+  if (dmgTgt === `originalTarget`) {
+    const victimId = html.find(".chat-target").last().data("id");
+    if (victimId) {
+      (async () => {
+        const actor = ((await fromUuid(victimId)) as TokenDocument)?.actor;
+        if (actor instanceof OseActor) {
+          await actor.applyDamage(amount, multiplier);
+        } else {
+          ui.notifications?.error(`Can't deal damage to ${victimId}`);
+        }
+      })();
+    } else {
+      ui.notifications?.error(`Can't find original target to deal damage`);
     }
-    if (dmgTgt === `selected`) {
-      canvas.tokens?.controlled.forEach(async (t) => {
-        if (game.user?.isGM && t.actor instanceof OseActor)
-          await t.actor.applyDamage(amount, multiplier);
-      });
-    }
+  }
+  if (dmgTgt === `targeted`) {
+    game.user?.targets.forEach(async (t) => {
+      if (game.user?.isGM && t.actor instanceof OseActor)
+        await t.actor.applyDamage(amount, multiplier);
+    });
+  }
+  if (dmgTgt === `selected`) {
+    canvas.tokens?.controlled.forEach(async (t) => {
+      if (game.user?.isGM && t.actor instanceof OseActor)
+        await t.actor.applyDamage(amount, multiplier);
+    });
   }
 }
 

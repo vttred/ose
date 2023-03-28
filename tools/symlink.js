cb178ad96d1e37abb8a99ac2a03ca3a8d93b8a3c
bakbakbakbakbak <bakbakbakbak@protonmail.com>
2023-03-28 19:56:06 +0200

    fix(symlink): support path array

--- tools/symlink.js
+++ tools/symlink.js
@@ -22,31 +22,41 @@ const foundryConfig = fs.readJSONSync(
   path.resolve(__dirname, "..", "foundryconfig.json")
 );
 
-const linkDirectory = path.resolve(
-  foundryConfig.dataPath,
-  "Data",
-  "systems",
-  foundryConfig.symLinkName
-);
-
 function createSymlink() {
-  console.log(`Linking dist to ${linkDirectory}.`);
-  const systemsDirectory = path.resolve(linkDirectory, "..");
-  if (!fs.pathExistsSync(systemsDirectory)) {
-    throw new Error(
-      `Link directory parent folder "${systemsDirectory}" not found.`
+  foundryConfig.dataPath.forEach((dataPath) => {
+     const linkDirectory = path.resolve(
+      dataPath,
+      "Data",
+      "systems",
+      foundryConfig.symLinkName
     );
-  }
+    
+    console.log(`Linking dist to ${linkDirectory}.`);
+    const systemsDirectory = path.resolve(linkDirectory, "..");
+    if (!fs.pathExistsSync(systemsDirectory)) {
+      throw new Error(
+        `Link directory parent folder "${systemsDirectory}" not found.`
+      );
+    }
 
-  if (!fs.existsSync(linkDirectory)) {
-    // link path doesn't exist, create it.
-    fs.symlinkSync(path.resolve(__dirname, ".."), linkDirectory);
-  }
+    if (!fs.existsSync(linkDirectory)) {
+      // link path doesn't exist, create it.
+      fs.symlinkSync(path.resolve(__dirname, ".."), linkDirectory);
+    }
+  });
 }
 
 function removeSymlink() {
-  console.log(`Removing build in ${linkDirectory}.`);
-  fs.removeSync(linkDirectory);
+  foundryConfig.dataPath.forEach((dataPath) => {
+    const linkDirectory = path.resolve(
+      dataPath,
+      "Data",
+      "systems",
+      foundryConfig.symLinkName
+    );
+    console.log(`Removing build in ${linkDirectory}.`);
+    fs.removeSync(linkDirectory);
+  });
 }
 
 if (argv.clean) {

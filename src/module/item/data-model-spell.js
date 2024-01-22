83512369bfd33e31c6eb60efe2f1a8a5289fd5f4
Apewall <1915182+apewall@users.noreply.github.com>
2024-01-21 21:48:36 -0600

    Fix roll formula not being displayed in roll tags

--- src/module/item/data-model-spell.js
+++ src/module/item/data-model-spell.js
@@ -22,15 +22,21 @@ export default class OseDataModelSpell extends foundry.abstract.DataModel {
   get #rollTag() {
     if (!this.roll) return null;
 
+    const formulaData = {
+      actor: this.parent.actor,
+      data: this._source
+    }
+
+    const rollFormula = new Roll(this.roll, formulaData).formula
+
     const rollTarget =
       this.rollTarget === undefined
         ? ""
         : ` ${CONFIG.OSE.roll_type[this.rollType]}${this.rollTarget}`;
 
     return {
-      label: `${game.i18n.localize("OSE.items.Roll")} ${
-        this.roll
-      }${rollTarget}`,
+      label: `${game.i18n.localize("OSE.items.Roll")} ${rollFormula
+        }${rollTarget}`,
     };
   }
 

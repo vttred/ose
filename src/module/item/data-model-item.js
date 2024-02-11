0fdc68344fe4473b528403f34199624f58ecb035
Henrik Bonsmann <112704394+Henrik-Bonsmann@users.noreply.github.com>
2024-02-11 13:55:10 +0100

    Derive NetValues for items.
    *To avoid excessive handlebar maths in the inventory tab, derive the product of quantity and (weight, cost & itemslots) in the item dataModel.
    
    * Set default quantity of new items to 1 to avoid confusion.

--- src/module/item/data-model-item.js
+++ src/module/item/data-model-item.js
@@ -2,6 +2,11 @@
  * @file The data model for Items of type Ability
  */
 export default class OseDataModelItem extends foundry.abstract.DataModel {
+  prepareDerivedData() {
+    this.netWeight = this.weight * this.quantity.value;
+    this.netCost = this.cost * this.quantity.value;
+    this.netItemslots = Math.ceil(this.itemslots * this.quantity.value);
+  }
   static defineSchema() {
     const {
       SchemaField,
@@ -19,11 +24,14 @@ export default class OseDataModelItem extends foundry.abstract.DataModel {
       cost: new NumberField({ min: 0, initial: 0 }),
       containerId: new StringField(),
       quantity: new SchemaField({
-        value: new NumberField({ min: 0, initial: 0 }),
+        value: new NumberField({ min: 0, initial: 1 }),
         max: new NumberField({ min: 0, initial: 0 }),
       }),
       weight: new NumberField({ min: 0, initial: 0 }),
       itemslots: new NumberField({ min: 0, initial: 0 }),
+      netWeight: new NumberField({ min: 0, initial: 0 }),
+      netCost: new NumberField({ min: 0, initial: 0 }),
+      netItemslots: new NumberField({ min: 0, initial: 0 }),
     };
   }
 

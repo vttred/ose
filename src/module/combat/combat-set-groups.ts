d6c2c22490034579db3d462b7d5bbe8f9421a669
arcos <andrea@arcosdev.com>
2024-09-14 16:06:29 +0200

    Reverted prettier formatting

--- src/module/combat/combat-set-groups.ts
+++ src/module/combat/combat-set-groups.ts
@@ -1,76 +1,79 @@
 import OSE from "../config";
 import { colorGroups } from "./combat-group";
 
-const { HandlebarsApplicationMixin, ApplicationV2 } = foundry.applications.api;
-
-export default class OSECombatGroupSelector extends HandlebarsApplicationMixin(
+const {
+  HandlebarsApplicationMixin,
   ApplicationV2
-) {
+} = foundry.applications.api;
+
+export default class OSECombatGroupSelector extends HandlebarsApplicationMixin(ApplicationV2) {
   _highlighted;
+
 
   // ===========================================================================
   // APPLICATION SETUP
   // ===========================================================================
   static DEFAULT_OPTIONS = {
-    id: "combat-set-groups-{id}",
-    classes: ["combat-set-groups", "scrollable"],
-    tag: "form",
-    window: {
-      frame: true,
-      positioned: true,
-      title: "OSE.combat.SetCombatantGroups",
-      icon: "fa-flag",
-      controls: [],
-      minimizable: false,
-      resizable: true,
-      contentTag: "section",
-      contentClasses: [],
-    },
-    actions: {},
-    form: {
-      handler: undefined,
-      submitOnChange: true,
-    },
-    position: {
-      width: 330,
-      height: "auto",
-    },
-  };
+      id: "combat-set-groups-{id}",
+      classes: ["combat-set-groups", "scrollable"],
+      tag: "form",
+      window: {
+        frame: true,
+        positioned: true,
+        title: "OSE.combat.SetCombatantGroups",
+        icon: "fa-flag",
+        controls: [],
+        minimizable: false,
+        resizable: true,
+        contentTag: "section",
+        contentClasses: []
+      },
+      actions: {},
+      form: {
+        handler: undefined,
+        submitOnChange: true
+      },
+      position: {
+        width: 330,
+        height: "auto"
+      }
+    }
 
   static PARTS = {
     main: {
-      template: `/systems/${game.system.id}/dist/templates/apps/combat-set-groups.hbs`,
-    },
-  };
+      template: `/systems/${game.system.id}/dist/templates/apps/combat-set-groups.hbs`
+    }
+  }
+
 
   // ===========================================================================
   // RENDER SETUP
-  // ===========================================================================
-
-  async _prepareContext(_options) {
+@@ -54,48 +51,46 @@ export default class OSECombatGroupSelector extends HandlebarsApplicationMixin(A
     return {
       groups: colorGroups,
       combatants: game.combat.combatants,
-    };
+    }
   }
 
   _onRender(context, options) {
     super._onRender(context, options);
-    for (const li of this.element.querySelectorAll("[data-combatant-id]")) {
+    for ( const li of this.element.querySelectorAll("[data-combatant-id]") ) {
       li.addEventListener("mouseover", this.#onCombatantHoverIn.bind(this));
       li.addEventListener("mouseout", this.#onCombatantHoverOut.bind(this));
     }
     this.element.addEventListener("change", this._updateObject);
   }
 
+
   // ===========================================================================
   // UPDATING
   // ===========================================================================
 
   protected async _updateObject(event: Event): Promise<void> {
-    const combatant = game.combat.combatants.get(event.target.name);
-    await combatant.setFlag(game.system.id, "group", event.target.value);
+    const combatant = game.combat.combatants.get(event.target.name);   
+    await combatant.setFlag(game.system.id, "group", event.target.value)
   }
+
 
   // ===========================================================================
   // UI EVENTS
@@ -78,19 +81,19 @@ export default class OSECombatGroupSelector extends HandlebarsApplicationMixin(
 
   #onCombatantHoverIn(event) {
     event.preventDefault();
-    if (!canvas.ready) return;
+    if ( !canvas.ready ) return;
     const li = event.currentTarget;
     const combatant = game.combat.combatants.get(li.dataset.combatantId);
     const token = combatant.token?.object;
-    if (token?.isVisible) {
-      if (!token.controlled) token._onHoverIn(event, { hoverOutOthers: true });
+    if ( token?.isVisible ) {
+      if ( !token.controlled ) token._onHoverIn(event, {hoverOutOthers: true});
       this._highlighted = token;
     }
   }
 
   #onCombatantHoverOut(event) {
     event.preventDefault();
-    if (this._highlighted) this._highlighted._onHoverOut(event);
+    if ( this._highlighted ) this._highlighted._onHoverOut(event);
     this._highlighted = null;
   }
 }

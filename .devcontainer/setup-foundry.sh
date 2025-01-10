e66c2b60197a827cf82bbb8149eb4d2bb87cad63
Antoni Sobkowicz <rughalt@gmail.com>
2025-01-10 18:50:55 +0000

    Fix caching issue and add neovim to devcontainer

--- .devcontainer/setup-foundry.sh
+++ .devcontainer/setup-foundry.sh
@@ -4,9 +4,10 @@ if [ ! -d "/home/node/.foundryvtt" ]; then
     # Check if there is a .foundrycache folder in the workspace and it has at least one file
     if [ -d "/workspaces/ose/.foundrycache" ] && [ "$(ls -A /workspaces/ose/.foundrycache)" ]; then
         # Find the newest file in the .foundrycache directory
-        foundryFile=$(ls -t /workspaces/ose/.foundrycache/*.zip | head -1)
-        echo "Using cached FoundryVTT file $foundryFile"
-        cp "$foundryFile" foundry.zip
+        cachedFoundryFile=$(ls -t /workspaces/ose/.foundrycache/*.zip | head -1)
+        echo "Using cached FoundryVTT file $cachedFoundryFile"
+        cp "/workspaces/ose/.foundrycache/$(basename "$cachedFoundryFile")" .
+        foundryFile=$(basename "$cachedFoundryFile")
     else
         echo 'Please provide the FoundryVTT timed URL from https://foundryvtt.com/. Select the Linux/Node.js operating system.'
         read -p 'FoundryVTT URL: ' foundryUrl
@@ -17,8 +18,6 @@ if [ ! -d "/home/node/.foundryvtt" ]; then
         echo "Copying $foundryFile to .foundrycache"
         mkdir -p /workspaces/ose/.foundrycache
         cp "$foundryFile" /workspaces/ose/.foundrycache
-        # wait for input
-        echo "Press any key to continue"
     fi
     # quietly unzip the file
     echo "Unzipping $foundryFile"
